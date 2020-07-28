3c45cffeb410bb9ed804148e36fbd5856d3c671a
elfulenw <elfulenw@gmail.com>
2020-07-27 22:49:24 -0400

    updated city weather analysis with DataFrames and plots

---
+++ VacationPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "YOUR KEY HERE!"
+
+# Google API Key
+g_key = "AIzaSyCwKhoYRpkTbmP2U8s0oq6Zr6qL1KUoc_4"
--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "d83ce551edc3e45cfaa435fdd61ca8e1"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyChV4icakoTem4bmQBz7CJ8h1BJbjfNxVM"
